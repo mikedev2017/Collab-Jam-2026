fda746e2bfc8cf00d074c41ea6dbce0c91b86d15
Aric Akbani <aricakbani@gmail.com>
2026-05-15 21:50:37 -0500

    Updated cutscene logic

--- scripts/cutscene_scripts/intro_cutscene.gd
+++ scripts/cutscene_scripts/intro_cutscene.gd
@@ -2,7 +2,6 @@ extends Node2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	animation_player.play("introduction")

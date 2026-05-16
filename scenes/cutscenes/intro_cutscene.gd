d62b205c4f0c79595146fc8a6a5d55e44cfb67e5
Aric Akbani <aricakbani@gmail.com>
2026-05-15 19:04:16 -0500

    Intro Cutscene and code added
    Intro cutscenes created.  Animation player node controls the screens.  SceneLoader transition used for entering into Main Menu.  Intro cutscene can be stopped early with the Left Mouse, Space or Enter keys.

---
+++ scenes/cutscenes/intro_cutscene.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	animation_player.play("test")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	print(animation_player.is_playing())
+	
+	if Input.is_action_just_pressed("skip_cutscene"):
+		SceneLoader.load_scene("res://scenes/main_menu.tscn")
+	
+	if !animation_player.is_playing():
+		SceneLoader.load_scene("res://scenes/main_menu.tscn")
